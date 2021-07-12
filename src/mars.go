f21c69020abfbed9c24710d6aa7eeda59aa055df
Kevin <zuoyenan@gmail.com>
2021-07-13 00:07:11 +0800

    add:some computation code

---
+++ src/mars.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("My weight on the surface of Mars is ")
+	fmt.Print(120.0 * 0.3783)
+
+	fmt.Print(" lbs, and I would be ")
+	fmt.Print(27 * 365 / 678)
+	fmt.Printf(" years old")
+
+}
