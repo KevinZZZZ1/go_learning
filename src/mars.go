ef3cf11276383a9d4996db137a6e58d63dbe3ef2
Kevin <zuoyenan@gmail.com>
2021-07-13 23:41:53 +0800

    fmt.println格式化

--- src/mars.go
+++ src/mars.go
@@ -8,6 +8,13 @@ func main() {
 
 	fmt.Print(" lbs, and I would be ")
 	fmt.Print(27 * 365 / 678)
-	fmt.Printf(" years old")
+	fmt.Print(" years old")
+
+	fmt.Println("My weight on the surface of Mars is", 120.0*0.3783, "lbs, and I would be", 27*365.2425/687, "years old.")
+
+	fmt.Printf("My weight on the surface of Mars is %v lbs", 120.0 * 0.3783)
+	fmt.Printf(" and I would be %v years old.\n", 27.0 * 365 / 687)
+
+	fmt.Printf("My weight on the surface of %v is %v lbs.\n", "Earth", 120.0)
 
 }
