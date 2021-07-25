420e3fadf456dbe113fab3c2655c29731c6d6062
Kevin <zuoyenan@gmail.com>
2021-07-25 21:50:14 +0800

    usage of OR and AND

--- src/mars.go
+++ src/mars.go
@@ -4,6 +4,16 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, World")
+	fmt.Println("The year is 2100, should you leap?")
+
+	var year = 2100
+
+	var leap = (year%400 == 0) || (year%4 == 0 && year%100 != 0)
+
+	if leap {
+		fmt.Println("Look before you leap!")
+	} else {
+		fmt.Println("Keep you feet on the ground.")
+	}
 
 }
