2e476b0ace7652f28eebbcfb0a868c6c4a984799
Kevin <zuoyenan@gmail.com>
2021-07-27 09:52:03 +0800

    usage of funcation

--- src/mars.go
+++ src/mars.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	kelvin := 294.0
+	celsius := kelvinToCelsius(kelvin)
 
-	fmt.Println("Hello, World")
+	fmt.Print("kelvin is ", kelvin, ", celsius is ", celsius)
+}
 
+func kelvinToCelsius(k float64) float64 {
+	k -= 273.15
+	return k
 }
