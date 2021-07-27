a6cea6100bb8bf3184d1492718ee172b40314ff5
Kevin <zuoyenan@gmail.com>
2021-07-27 10:02:30 +0800

    方法的使用

--- src/mars.go
+++ src/mars.go
@@ -2,8 +2,25 @@ package main
 
 import "fmt"
 
+type celsius float64
+
+type fahrenheit float64
+
+type kelvin float64
+
 func main() {
 
 	fmt.Println("Hello, World")
 
+}
+
+// func关键字 (接收者 接收者类型) 方法名(参数列表) 返回值类型
+func (c celsius) toFahrenheit() fahrenheit {
+
+	return fahrenheit((c*9)/5 + 32)
+
+}
+
+func (c celsius) toKelvin() kelvin {
+	return kelvin(c + 272.15)
 }
