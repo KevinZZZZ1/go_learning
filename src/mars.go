2de4c4aa0814b42ed793e6a8da603dd601454fc1
Kevin <zuoyenan@gmail.com>
2021-07-27 23:56:01 +0800

    使用闭包

--- src/mars.go
+++ src/mars.go
@@ -2,8 +2,25 @@ package main
 
 import "fmt"
 
+// 因为函数字面值（即匿名函数）要保留外部作用域的变量引用，所以函数字面值都是闭包的
+// 闭包就是由于匿名函数封闭并包围作用域中的变量而得名的
+
+type kelvin float64
+
+type sensor func() kelvin
+
 func main() {
+	var sensor sensor = calibrate(realSensor, 5)
+	fmt.Println(sensor())
+}
 
-	fmt.Println("Hello, World")
+func realSensor() kelvin {
+	return 0
+}
 
+func calibrate(s sensor, offset kelvin) sensor {
+	return func() kelvin {
+		// 这里能获取到当前匿名函数作用域之外的参数，即保留了外部作用域的变量
+		return offset + s()
+	}
 }
