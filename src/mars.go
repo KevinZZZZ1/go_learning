cf0a6950cdd9b8b3ee97f860fe061acd67d86224
Kevin <zuoyenan@gmail.com>
2021-07-27 23:37:46 +0800

    init 闭包和匿名函数

--- src/mars.go
+++ src/mars.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+// 匿名函数就是没有名字的函数，在GO里也可以称为函数字面值
+
+var f func() = func() {
+	fmt.Println("Dress up for the masquerade")
+}
+
 func main() {
 
-	fmt.Println("Hello, World")
+	f()
 
 }
