5c4610e9af2acbffdbd7c1ac36088c61f034862d
Kevin <zuoyenan@gmail.com>
2021-07-31 20:41:00 +0800

    map的使用

--- src/mars.go
+++ src/mars.go
@@ -4,6 +4,21 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, World")
+	// 声明一个<string, int>的map
+	var temperature map[string]int = map[string]int{
+		"Earth": 15,
+		"Mars":  -65,
+	}
 
+	// 获得Key为Earth对应的Value
+	temp := temperature["Earth"]
+	fmt.Printf("On average the Earth is %v.\n", temp)
+
+	// 设置Key为Earth，Value为16到map中
+	temperature["Earth"] = 16
+	temperature["Venus"] = 464
+	fmt.Println(temperature)
+
+	moon := temperature["Moon"]
+	fmt.Println(moon)
 }
