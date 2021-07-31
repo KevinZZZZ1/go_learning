063bb18bc3d3c8b7a2cc4c4da862c8ca1b3d3758
Kevin <zuoyenan@gmail.com>
2021-07-31 21:26:47 +0800

    map赋值以及进行添加或删除操作

--- src/mars.go
+++ src/mars.go
@@ -4,6 +4,22 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, World")
+	var planets map[string]string = map[string]string{
+		"Earth": "Sector ZZ9",
+		"Mars":  "Sector ZZ9",
+	}
+
+	// map在赋值的时候并不会新建副本赋值，而是直接将对象赋值过去
+	planetsMarkII := planets
+
+	// 这里会把planets和planetsMarkII的Key为Earth的Value都设置为whoops
+	planets["Earth"] = "whoops"
+	fmt.Println(planets)
+	fmt.Println(planetsMarkII)
+
+	// 在map中删除对应key
+	delete(planets, "Earth")
+	fmt.Println(planetsMarkII)
+	fmt.Println(planets)
 
 }
