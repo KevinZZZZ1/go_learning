6f9019f21e6782b8446a3ff161d8e857ff571652
Kevin <zuoyenan@gmail.com>
2021-08-01 16:10:02 +0800

    指针的声明和使用

--- src/mars.go
+++ src/mars.go
@@ -4,6 +4,17 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, World")
+	// 指针是指向另一个变量地址的变量
+
+	// &表示地址操作符，通过&可以获得变量的内存地址
+	// *和&的作用相反，是用来解引用的，表示得到内存地址指向的值
+
+	var answer int32 = 42
+	fmt.Println(&answer)
+
+	// 定义一个指向int32类型的指针，*放在变量前面表示解引用操作，放在类型前面表示指针类型
+	var address *int32 = &answer
+	fmt.Printf("%T\n", address)
+	fmt.Println(*address)
 
 }
