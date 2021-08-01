a9fe3761b90c09922bf9a325b8e92d374765031c
Kevin <zuoyenan@gmail.com>
2021-08-01 11:00:05 +0800

    struct的初始化和格式化打印

--- src/mars.go
+++ src/mars.go
@@ -6,29 +6,25 @@ func main() {
 
 	// struct可以将多个不同类型的字段组合到一起
 
-	// 声明了一个变量curiosity，它的类型是struct
-	// 这种声明类似Java中的匿名类
-	var curiosity struct {
-		lat  float64
-		long float64
-	}
-
-	curiosity.lat = -4.5895
-	curiosity.long = 137.4417
-
-	fmt.Println(curiosity.lat, curiosity.long)
-	fmt.Println(curiosity)
-
 	// 这里是声明了一个location类型的struct，这样就能符用这个类型
 	type location struct {
 		lat  float64
 		long float64
 	}
 
-	var spririt location
-	spririt.lat = -14.5684
-	spririt.long = 175.472636
+	// 初始化struct，使用成对的key,value的形式初始化
+	// 如果字段不设置初始化值的话，会使用字段类型的零值
+	var spririt location = location{
+		lat:  -1.9462,
+		long: 354.4734,
+	}
 
-	fmt.Println(spririt)
+	// 初始化struct的另一种方式，没有显式指定字段名及其值，会以字段的声明顺序进行赋值
+	var opportunity location = location{-17.5698, 175.45521}
+
+	// 格式化打印struct
+	fmt.Printf("%+v\n", opportunity)
+	fmt.Printf("%v\n", spririt)
+
 
 }
